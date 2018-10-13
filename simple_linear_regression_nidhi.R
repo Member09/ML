1acc6cead0c394154b7256d4a6c44f6006a75f88
Member09 <Member09@users.noreply.github.com>
2018-10-13 20:48:30 +0530

    Updated the linear regression

--- simple_linear_regression_nidhi.R
+++ simple_linear_regression_nidhi.R
@@ -13,3 +13,25 @@ regressor = lm(formula = Salary~YearsExperience, training_set)
 #predicting test set results
 y_pred = predict(regressor, newdata = test_set)
 
+#Visualization
+#install.packages('ggplot2')
+library('ggplot2')
+#library('caTools')
+ggplot() +
+  geom_point(aes(x=training_set$YearsExperience, y=training_set$Salary),
+             colour='red') +
+  geom_line(aes(x=training_set$YearsExperience, y= predict(regressor, newdata=training_set)),
+            colour='blue') +
+  ggtitle('Salary Vs Experience(Training Set)')+
+  xlab('Years of Experience') +
+  ylab('Salary')
+
+
+ggplot() +
+  geom_point(aes(x=test_set$YearsExperience, y=test_set$Salary),
+             colour='red') +
+  geom_line(aes(x=training_set$YearsExperience, y= predict(regressor, newdata=training_set)),
+            colour='blue') +
+  ggtitle('Salary Vs Experience(Test Set)')+
+  xlab('Years of Experience') +
+  ylab('Salary')
